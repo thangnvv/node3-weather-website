89ccaa56a6fc4081c4c9b54510fe6d34e1bb706c
thangnvv <thangnvv2010@gmail.com>
2022-05-22 05:59:55 +0700

    Add humidity to Weather page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,6 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, address, callback) => {
-    // const url = 'http://api.weatherstack.com/current?access_key=60bbec4255c373422a125858757ec13f&query=' + latitude + ',' + longitude + '&units=f'
     const url = 'http://api.weatherstack.com/current?access_key=60bbec4255c373422a125858757ec13f&query=' + address + '&units=f'
 
     request({url, json: true}, (error, {body}) => {
@@ -10,7 +9,7 @@ const forecast = (latitude, longitude, address, callback) => {
         } else if (body.error) {
             callback(undefined, 'Unable to find location')
         } else {
-            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.');
+            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Humidity: ' + body.current.humidity);
         }
     })
 }
